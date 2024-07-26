0aa5558422366b5b8100a5af0a10144ef2dae4a7
ckccyrus <>
2024-07-26 14:00:42 -0400

    dev: add lucide-react for icon; add custom buttons component; add pageHeader layout;

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { PageHeader } from './layouts/PageHeader'
 
+const App = () => {
   return (
-    <div>Youtube</div>
-    
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+    </div>
+
   )
 }
 
