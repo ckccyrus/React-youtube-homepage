f07803149124fe2ee117525ae5e2300026557b7e
ckccyrus <>
2024-07-26 23:29:23 -0400

    dev: add video section; add custom utils to format post date and duration;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 
-import { categories } from './data/home'
+import { useState } from 'react'
+
+import { categories, videos } from './data/home'
 
 import { PageHeader } from './layouts/PageHeader'
 import { CategoryPills } from './components/CategoryPills'
-import { useState } from 'react'
+import { VideoGridItem } from './components/VideoGridItem'
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -17,6 +19,12 @@ const App = () => {
         <div className='overflow-x-hidden px-8 pb-4'>
           <div className='sticky top-0 bg-white z-10 pb-4'>
             <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+          </div>
+          <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+
+            ))}
           </div>
         </div>
 
